efde986ad870a272200917c60d7634b23340bcd4
hby-soda <sodagreen_forever@qq.com>
2019-02-23 10:36:49 +0800

    add weekend

---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,79 @@
+<template>
+    <div>
+      <div class="title">周末去哪儿</div>
+      <ul>
+        <li class="item" v-for="item of weekendList" :key="item.id">
+          <div class="item-img-wrapper">
+            <img class="item-img" :src="item.imgUrl"/>
+          </div>
+          <div class="item-info">
+            <p class="item-title">{{item.title}}</p>
+            <p class="item-desc">{{item.desc}}</p>
+          </div>
+        </li>
+      </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      weekendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1811/f3/86173f863bef61.jpg_r_640x214_52b003ac.jpg',
+        title: '京城周末撒欢',
+        desc: '在帝都过周末，不仅仅是城中游！'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1811/f3/86173f863bef61.jpg_r_640x214_52b003ac.jpg',
+        title: '京城周末撒欢',
+        desc: '在帝都过周末，不仅仅是城中游！'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1811/f3/86173f863bef61.jpg_r_640x214_52b003ac.jpg',
+        title: '京城周末撒欢',
+        desc: '在帝都过周末，不仅仅是城中游！'
+      }, {
+        id: '0004',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1811/f3/86173f863bef61.jpg_r_640x214_52b003ac.jpg',
+        title: '京城周末撒欢',
+        desc: '在帝都过周末，不仅仅是城中游！'
+      }, {
+        id: '0005',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1811/f3/86173f863bef61.jpg_r_640x214_52b003ac.jpg',
+        title: '京城周末撒欢',
+        desc: '在帝都过周末，不仅仅是城中游！'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .title
+    margin-top .2rem
+    line-height .8rem
+    background #eee
+    text-indent .2rem
+  .item
+    overflow hidden
+    height 4.04rem
+    .item-img-wrapper
+      overflow hidden
+      height 0
+      padding-bottom 36.5%
+      .item-img
+        width 100%
+    .item-info
+      padding .1rem 0.2rem
+      .item-title
+        line-height .54rem
+        color #212121
+        ellipsis()
+      .item-desc
+        line-height .4rem
+        color #616161
+        ellipsis()
+</style>
